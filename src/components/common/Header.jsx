a84424821f1b63763f44aad86421ba069dd254ac
Jimin <jimin4124.22@gmail.com>
2024-03-12 13:20:13 +0900

    상단 navbar 레이아웃

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,26 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "~/styles/Header.css";
 
-function BasicExample() {
+function Header() {
     return (
         <>
-            <Navbar expand="sm" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand href="#home">RE:search</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">홈</Nav.Link>
-                            <Nav.Link href="#analyst">애널리스트</Nav.Link>
-                            <Nav.Link href="#company">증권사</Nav.Link>
-                            <Nav.Link href="#bookmark">즐겨찾기</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+            <Navbar className="fixed-top">
+                <Container className="custom-navbar">
+                    {/* <Navbar.Brand href="#home">RE:search</Navbar.Brand> */}
+                    <Nav>
+                        <Nav.Link href="#home">홈</Nav.Link>
+                        <Nav.Link href="#analyst">애널리스트</Nav.Link>
+                        <Nav.Link href="#company">증권사</Nav.Link>
+                        <Nav.Link href="#bookmark">즐겨찾기</Nav.Link>
+                    </Nav>
                 </Container>
             </Navbar>
         </>
     );
 }
 
-export default BasicExample;
+export default Header;
