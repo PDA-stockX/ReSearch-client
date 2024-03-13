aec208be919fe0cf3ce367d908092404cc880175
kim hyun soo <hyjkl0124@naver.com>
2024-03-13 16:48:25 +0900

    fix socket.io

---
+++ src/Analyst/AnalystCard.jsx
@@ -0,0 +1,16 @@
+import React, { useCallback, useEffect, useState } from "react";
+import "./AnalystDetail.css";
+export default function AnalystCard(props) {
+  return (
+    <>
+      <div className="cardBox ">
+        <div>asdf</div>
+        <div className="col">
+          <div>asdf</div>
+          <div>sdaf</div>
+          <div>asdf</div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Analyst/AnalystMessage.jsx
+++ src/Analyst/AnalystMessage.jsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
-
+import "./AnalystDetail.css";
 export default function AnalystMessage(props) {
   return (
     <div>
       {console.log(props + "asd")}
-      <h3>{props.chat}</h3>
+      <h3 className="messageBox rounded-lg">{props.chat}</h3>
     </div>
   );
 }
