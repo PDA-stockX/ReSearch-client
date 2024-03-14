99df68bd20f1e442c7cf028f7ce82ab63f2499e4
Jimin <jimin4124.22@gmail.com>
2024-03-14 13:25:14 +0900

    font 적용 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import mainRouter from "~/routers/main-router";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-`;
 
 function App() {
   return (
     <>
-      <GlobalStyle />
       <RouterProvider router={mainRouter}/>
     </>
   );
