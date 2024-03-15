390e5844640e7fca4b6a52afb6d557d0ff2114a5
jinhyeon <vmflwu123@gmail.com>
2024-03-15 15:35:37 +0900

    feat: 로그인 및 회원가입 뷰 컴포넌트 추가

--- src/pages/analyst/AchievementScore.jsx
+++ src/pages/analyst/AchievementScore.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function AnalystAchievementRate() {
+export default function AchievementRate() {
     return (
         <div>AchievementRate</div>
     )
--- src/pages/firm/ReturnRate.jsx
+++ src/pages/firm/ReturnRate.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function CompanyEarningRate() {
+export default function ReturnRate() {
     return (
         <div>EarningRate</div>
     )
