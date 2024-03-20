cee387cbdaabcb287244c63f629728d4ae45ef11
jinhyeon <vmflwu123@gmail.com>
2024-03-20 09:04:44 +0900

    feat: side navigation bar 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,62 +1,67 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useLocation, useNavigate } from "react-router-dom";
-import "~/styles/Header.css";
+import {useLocation, useNavigate} from "react-router-dom";
 import {SearchModal} from "../SearchModal.jsx";
 import "./Common.css";
+import SideNavMenu from "~/components/common/SideNavMenu.jsx";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default function Header() {
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <Navbar className="fixed-top bg-white" sticky="top">
-        <Container className="custom-navbar">
-          <Nav>
-            <Nav.Link
-              onClick={() => {
-                navigate("/");
-              }}
-              className={location.pathname === "/home" ? "active" : ""}
-            >
-              홈
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                navigate("/analyst/return-rate");
-              }}
-              className={
-                location.pathname.startsWith("/analyst") ? "active" : ""
-              }
-            >
-              애널리스트
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                navigate("/firm/return-rate");
-              }}
-              className={
-                location.pathname.startsWith("/firm") ? "active" : ""
-              }
-            >
-              증권사
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                navigate("/bookmark/analyst");
-              }}
-              className={
-                location.pathname.startsWith("/bookmark") ? "active" : ""
-              }
-            >
-              즐겨찾기
-            </Nav.Link>
-          </Nav>
-        </Container>
-        <SearchModal isMobile={true}></SearchModal>
-      </Navbar>
-    </>
-  );
+    return (
+        <>
+            <Navbar className="fixed-top bg-white" sticky="top">
+                <Container className="custom-navbar">
+                    <Nav>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate("/");
+                            }}
+                            className={location.pathname === "/home" ? "active" : ""}
+                        >
+                            홈
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate("/analyst/return-rate");
+                            }}
+                            className={
+                                location.pathname.startsWith("/analyst") ? "active" : ""
+                            }
+                        >
+                            애널리스트
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate("/firm/return-rate");
+                            }}
+                            className={
+                                location.pathname.startsWith("/firm") ? "active" : ""
+                            }
+                        >
+                            증권사
+                        </Nav.Link>
+                        <Nav.Link
+                            onClick={() => {
+                                navigate("/bookmark/analyst");
+                            }}
+                            className={
+                                location.pathname.startsWith("/bookmark") ? "active" : ""
+                            }
+                        >
+                            즐겨찾기
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+                <SearchModal isMobile={true}></SearchModal>
+                <SideNavMenu placement="end" name="end" />
+            </Navbar>
+        </>
+    );
 }
