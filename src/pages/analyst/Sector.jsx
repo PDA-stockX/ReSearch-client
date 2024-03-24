1e45ba1b3ed96c3709f48d163dd66ee71604e17e
Jimin <jimin4124.22@gmail.com>
2024-03-24 14:55:34 +0900

    feat: analyst 업종별 순위 작업중

--- src/pages/analyst/Sector.jsx
+++ src/pages/analyst/Sector.jsx
@@ -1,21 +1,23 @@
-import React, { useState, useEffect, useParams } from 'react'
+import React, { useState, useEffect, useParams, useHistory } from 'react'
 import Best3 from '~/components/common/Best3';
 import Rank from '~/components/common/Rank';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { fetchSectorRank  } from '~/api/analysts';
+import { fetchReportSectors } from '~/api/reportSectors';
 import './analyst.css';
 
 
 export default function Sector() {
     const [data, setData] = useState([]);
     const [best, setBest] = useState([]);
-    const { sector } = useParams();
+    const [sectors, setSectors] = useState([]);
+    const { sector:selectedSector } = useParams();
+    const history = useHistory();
 
     useEffect(() => {
         async function fetchData() {
-            const ranking = await fetchSectorRank ();
-            console.log("랭킹", ranking)
+            const ranking = await fetchSectorRank(selectedSector);
             const data1 = ranking.map((item, index) => [
                 index + 1, 
                 item.name,
@@ -33,11 +35,22 @@ export default function Sector() {
 
             return {data1, data2}
         }
+
+        async function fetchSectorName() {
+            const names = await fetchReportSectors();
+            setSectors(names);
+        }
+
         fetchData().then(({data1, data2}) => {
             setData(data1);
             setBest(data2);
         });
-    }, [])
+        fetchSectorName();
+    }, [selectedSector])
+
+    const handleSectorChange = (sector) => {
+        history.push(`/analysts?sector=${sector}`);
+    };
 
     // 현재 날짜
     const today = new Date();
@@ -50,14 +63,19 @@ export default function Sector() {
         { columnName: '달성률', columnWidth: 60 } // 총 width = 240이 되도록?
     ];
 
+    // 기본 선택 항목
+    const defaultSector = sectors.length > 0 ? sectors[0] : '';
+
     return (
         <>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <h2>Best 3</h2>
                 <DropdownButton id="dropdown-basic-button" title={<ion-icon name="options-outline"></ion-icon>}>
-                    <Dropdown.Item href="#/action-1">업종1</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">업종2</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">업종3</Dropdown.Item>
+                    {sectors.map((sector, index) => (
+                        <Dropdown.Item key={index} onClick={() => handleSectorChange(sector)}>
+                            {sector}
+                        </Dropdown.Item>
+                    ))}
                 </DropdownButton>
             </div>
             <div
@@ -75,7 +93,7 @@ export default function Sector() {
                     justifyContent: "space-between",
                 }}
             >   
-                <h2>업종별 순위</h2>    {/* 업종별 대신 현재 선택한 업종 받아오기 */}
+                <h2>{selectedSector || defaultSector} 순위</h2>    {/* 업종별 대신 현재 선택한 업종 받아오기 */}
                 <h5>기준 날짜: {formattedDate}</h5>
             </div>
             <div
