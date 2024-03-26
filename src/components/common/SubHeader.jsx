f5a436f90c210b8b424fc590013a1ab976a868f2
Jimin <jimin4124.22@gmail.com>
2024-03-26 21:37:48 +0900

    merge

--- src/components/common/SubHeader.jsx
+++ src/components/common/SubHeader.jsx
@@ -1,33 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useLocation, useNavigate } from "react-router-dom";
-import { fetchReportSectors } from '~/api/reportSectors';
+import { fetchReportSectors } from "~/api/reportSectors";
 import "./Common.css";
 
 export default function SubHeader(props) {
   const location = useLocation();
   const navigate = useNavigate();
   const [sectors, setSectors] = useState([]);
-  
+
   useEffect(() => {
     async function fetchData() {
-        const data = await fetchReportSectors();
-        return data
+      const data = await fetchReportSectors();
+      return data;
     }
     fetchData().then((data) => {
-      setSectors(data)
-    })
+      setSectors(data);
+    });
   }, []);
 
-  
   // 기본 선택 업종
-  const defaultSector = sectors.length > 0 ? sectors[0]['sectorName'] : '';
+  const defaultSector = sectors.length > 0 ? sectors[0]["sectorName"] : "";
 
   return (
     <>
-      <Navbar className="fixed-top" bg="F7F7F7" sticky="top" style={{padding: 0}}>
+      <Navbar className="fixed-top" bg="F7F7F7" sticky="top" style={{ padding: 0 }}>
         <Container className="custom-navbar sub-navbar">
           <Nav>
             {console.log("여기", props, location.pathname)}
@@ -37,17 +36,12 @@ export default function SubHeader(props) {
                 key={index}
                 onClick={() => {
                   if (item === "sector") {
-                      navigate(`/${props.menu}?${item}=${defaultSector}`);
+                    navigate(`/${props.menu}?${item}=${defaultSector}`);
                   } else {
-                      navigate(`/${props.menu}/${item}`);
+                    navigate(`/${props.menu}/${item}`);
                   }
                 }}
-                className={
-                  location.pathname === `/${props.menu}/${item}` ||
-                  (item === "sector" && location.pathname === (`/${props.menu}`))
-                    ? "active"
-                    : ""
-                }
+                className={location.pathname === `/${props.menu}/${item}` || (item === "sector" && location.pathname === `/${props.menu}`) ? "active" : ""}
               >
                 {props.subMenuName[index]}
               </Nav.Link>
