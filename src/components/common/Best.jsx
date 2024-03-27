6bc32fc4474c72e970d10f3e88be29f142ec0bbc
Jimin <jimin4124.22@gmail.com>
2024-03-27 13:12:41 +0900

    feat: best, rank 컴포넌트 css 수정

--- src/components/common/Best.jsx
+++ src/components/common/Best.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import './Common.css'
+import React from "react";
+import "./Common.css";
 
-export default function Best({ name, company, returnRate }) {
-    return (
-        <div className="bestCard">
-            <div className="bestInfo">
-                <div className="name">{name}</div>
-                <div className="company">{company}</div>
-            </div>
-            <div className="returnRate">{returnRate}</div>
+export default function Best({ rank, name, company, returnRate }) {
+  const color = ["#d5a11e", "#A3A3A3", "#CD7F32"];
+  return (
+    <div className="bestCard">
+      <div className="bestInfo">
+        <div className="rank" style={{ color: `${color[rank - 1]}` }}>
+          {rank}
         </div>
-    )
+        <div className="name">{name}</div>
+        <div className="company">{company}</div>
+      </div>
+      {/* <div className="returnRate">{returnRate}</div> */}
+    </div>
+  );
 }
