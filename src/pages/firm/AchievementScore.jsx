a5b28bdb26d4581b723c2aa9209f3663c0cc7a51
ye-s-rin <pyr53540@gmail.com>
2024-03-28 09:15:27 +0900

    feat: 증권사 달성률 순위 페이지

--- src/pages/firm/AchievementScore.jsx
+++ src/pages/firm/AchievementScore.jsx
@@ -1,40 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import Best3 from '~/components/common/Best3';
 import Rank from '~/components/common/Rank';
 import './firm.css';
-
+import { fetchAchievementScoreRank } from "~/api/firms";
 
 export default function AchievementScore() {
-  const exampleColumn = [
-      { columnName: '순위', columnWidth: 40 }, 
-      { columnName: '증권사', columnWidth: 50 },
-      { columnName: '업종', columnWidth: 100 },
-      { columnName: '달성률', columnWidth: 50 }
-  ];
+    const [data, setData] = useState([]);
+    const [best, setBest] = useState([]);
 
-  const exampleData = [
-      [1, "Analyst1", "Sector1", 10],
-      [2, "Analyst2", "Sector2", 8],
-      [3, "Analyst3", "Sector3", 7],
-      [4, "Analyst4", "Sector4", 6],
-      [5, "Analyst5", "Sector5", 5],
-      [6, "Analyst5", "Sector6", 5],
-      [7, "Analyst5", "Sector7", 5],
-      [8, "Analyst5", "Sector8", 5],
-      [9, "Analyst5", "Sector9", 5],
-      [10, "Analyst5", "Sector10", 5],
-      [11, "Analyst5", "Sector11", 5],
-  ];
+    useEffect(() => {
+        async function fetchData() {
+            const ranking = await fetchAchievementScoreRank();
+            const data = ranking.map((item, index) => [
+                index + 1,
+                item.name,
+                item.achievementScore,
+            ]);
 
-  const exampleBest = [
-      ['최승환', '신한투자증권', '74.6'],
-      ['김철수', '미래에셋증권', '68.2'],
-      ['박지영', 'KB증권', '59.1']
-  ];
+            const top3 = ranking.slice(0, 3);
+            const best = top3.map((item, index) => [
+                index + 1,
+                item.name,
+                { name: "" },
+                item.achievementScore,
+            ]);
 
-  return (
-      <>
-        <h2>Best 3</h2>
+            return { data, best }
+        }
+        fetchData().then(({ data, best }) => {
+            setData(data);
+            setBest(best);
+        });
+    }, [])
+
+    const exampleColumn = [
+        { columnName: '순위', columnWidth: 40 },
+        { columnName: '증권사', columnWidth: 150 },
+        { columnName: '달성률', columnWidth: 50 }
+    ];
+
+    const exampleData = [
+        [1, "Firm1", 10],
+        [2, "Firm2", 8],
+        [3, "Firm3", 7],
+        [4, "Firm4", 6],
+        [5, "Firm5", 5],
+        [6, "Firm5", 5],
+        [7, "Firm5", 5],
+        [8, "Firm5", 5],
+        [9, "Firm5", 5],
+        [10, "Firm5", 5],
+        [11, "Firm5", 5],
+    ];
+
+    const exampleBest = [
+        ['', '신한투자증권', { name: "" }, '74.6'],
+        ['', '미래에셋증권', { name: "" }, '68.2'],
+        ['', 'KB증권', { name: "" }, '59.1']
+    ];
+
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+
+    return (
+        <>
+            <h2>Best 3</h2>
             <div
                 style={{
                     display: "flex",
@@ -42,9 +72,19 @@ export default function AchievementScore() {
                     alignItems: "center",
                 }}
             >
-                <Best3 data={exampleBest}></Best3>
+                {<Best3 data={exampleBest}></Best3>}
+                {/* {best.length > 0 && <Best3 data={best}></Best3>} */}
             </div>
-            <h2>달성률 순위</h2>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
+            >
+                <h2>달성률 순위</h2>
+                <h5>기준 날짜: {formattedDate}</h5>
+            </div>
             <div
                 style={{
                     display: "flex",
@@ -54,6 +94,6 @@ export default function AchievementScore() {
             >
                 <Rank column={exampleColumn} data={exampleData}></Rank>
             </div>
-      </>
-  )
+        </>
+    )
 }
