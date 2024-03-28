337e1560b8d244b885ef6d5d013092b3546ef854
ye-s-rin <pyr53540@gmail.com>
2024-03-28 13:33:41 +0900

    fix: 더미 데이터 삭제

--- src/pages/firm/AchievementScore.jsx
+++ src/pages/firm/AchievementScore.jsx
@@ -4,26 +4,6 @@ import Rank from '~/components/common/Rank';
 import './firm.css';
 import { fetchAchievementScoreRank } from "~/api/firms";
 
-const exampleData = [
-    [1, "Firm1", 10],
-    [2, "Firm2", 8],
-    [3, "Firm3", 7],
-    [4, "Firm4", 6],
-    [5, "Firm5", 5],
-    [6, "Firm5", 5],
-    [7, "Firm5", 5],
-    [8, "Firm5", 5],
-    [9, "Firm5", 5],
-    [10, "Firm5", 5],
-    [11, "Firm5", 5],
-];
-
-const exampleBest = [
-    ['', '신한투자증권', { name: "" }, '74.6'],
-    ['', '미래에셋증권', { name: "" }, '68.2'],
-    ['', 'KB증권', { name: "" }, '59.1']
-];
-
 const today = new Date();
 const formattedDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
 
@@ -72,8 +52,7 @@ export default function AchievementScore() {
                     alignItems: "center",
                 }}
             >
-                {<Best3 data={exampleBest}></Best3>}
-                {/* {best.length > 0 && <Best3 data={best}></Best3>} */}
+                {best.length > 0 && <Best3 data={best}></Best3>}
             </div>
             <div
                 style={{
