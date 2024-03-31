a9fc78361838abe48d1ec4411889ff2677ea3c45
Jimin <jimin4124.22@gmail.com>
2024-03-31 23:31:57 +0900

    feat: 검색창 검색결과 UI 및 detail page 연결

--- src/components/FirmInfo.jsx
+++ src/components/FirmInfo.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
+import "~/styles/SearchResult.css";
 
 export default function FirmInfo({ firm }) {
-    return (
-        <>
-            <div>
-                <h1>{firm.name}</h1>
-                <p>{firm.returnRate}</p>
-                <p>{firm.achievementScore}</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="result">
+        <p>이름: {firm.name}</p>
+        <p>수익률: {firm.returnRate}</p>
+        <p>달성점수: {firm.achievementScore}</p>
+      </div>
+    </>
+  );
 }
--- src/components/ReportInfo.jsx
+++ src/components/ReportInfo.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from "react";
 import "~/styles/ReportInfo.css";
 
 export default function ReportInfo({ report }) {
-    return (
-        <>
-            <div className="reportInfoContainer">
-                <div className="reportInfoHeader">
-                    <p>{report.title}</p>
-                    <p>{report.postedAt}</p>
-                </div>
-                <div className="reportInfoBody">
-                    <p>{report.achievementScore}</p>
-                    <p>{report.returnRate}</p>
-                </div>
-                <div className="reportInfoFooter">
-                    <p className="analyst">{report.analyst.name}</p>
-                    <p>{report.firm.name}</p>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="reportInfoContainer">
+        <div className="reportInfoHeader">
+          <p>{report.title}</p>
+          <p>{report.postedAt}</p>
+        </div>
+        <div className="reportInfoBody">
+          <p>{report.achievementScore}</p>
+          <p>{report.returnRate}</p>
+        </div>
+        <div className="reportInfoFooter">
+          <p className="analyst">{report.analyst.name}</p>
+          <p>{report.firm.name}</p>
+        </div>
+      </div>
+    </>
+  );
 }
