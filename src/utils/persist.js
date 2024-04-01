e346a811cd593c71d63d145acedd474a4fb32a00
jinhyeon <vmflwu123@gmail.com>
2024-04-01 14:32:02 +0900

    fix: compile error 처리

--- src/utils/persist.js
+++ src/utils/persist.js
@@ -1,5 +1,13 @@
 function getAuthContext() {
     const persistRoot = localStorage.getItem("persist:root");
+    if (!persistRoot) {
+        return {
+            isAuthenticated: false,
+            token: null,
+            user: null,
+        };
+    }
+
     const root = JSON.parse(persistRoot);
     const auth = JSON.parse(root.auth);
 
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_API_URL,
+        target: "http://15.165.71.109:80",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/,
             import.meta.env.mode === "production" ? "/api" : ""),
